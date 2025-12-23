ae9375b9bae2f30fc5192b50db871d3d53e8e1fc
keysLo <34831410+keysLo@users.noreply.github.com>
2025-12-23 17:08:29 +0800

    Fix duplicate React import in VideoCard

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, {useState} from 'react';
 import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
